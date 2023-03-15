6277ebd60c8f6e012bb327795df854a0aa744438
Einstainer <32267134+Einstainer@users.noreply.github.com>
2023-03-15 12:27:22 +0200

    Rudimentary basic API GET function with external TUF base data received, compiled and returned back in GET

---
+++ EntryProject_TUFAPI/Controllers/TUFAPIController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using EntryProject_TUFAPI.Models;
+//using EntryProject_TUFAPI.Models.DTO;
+//using EntryProject_TUFAPI.Data;
+
+namespace EntryProject_TUFAPI.Controllers
+{
+    [Route("api/TUFAPI")]
+    [ApiController]
+    public class TUFAPIController : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<TUF.registerDataStruct> GetTUF()
+        {
+            TUF newTUF = new TUF();
+            return newTUF.TUFRegisterList;
+            //return TUFStore._TUFList;
+        }
+    }
+}
---
+++ EntryProject_TUFAPI/Models/TUF.cs
@@ -0,0 +1,72 @@
+﻿using System.Text;
+using System.Net;
+
+namespace EntryProject_TUFAPI.Models
+{
+    public class TUF
+    {
+        private List<registerDataStruct> _TUFRegister = new List<registerDataStruct>();
+        private string _date;
+
+        public string date { get {return _date ;} }
+        public List<registerDataStruct> TUFRegisterList { get {return _TUFRegister;} }
+
+        public TUF()
+        {
+            GetTUFData();
+        }
+
+        private void GetTUFData()
+        {
+
+            using (WebClient client = new WebClient())
+            using (Stream stream = client.OpenRead("http://tuftuf.gambitlabs.fi/feed.txt"))
+            using (StreamReader streamR = new StreamReader(stream))
+            {
+                _date = streamR.ReadLine();
+
+                while (streamR.Peek() > -1)
+                {
+
+                    string streamRLine = streamR.ReadLine();
+                    char[] lineResult;
+                    StringBuilder idBuilder = new StringBuilder();
+                    StringBuilder bitIntegralBuilder = new StringBuilder();
+                    bool isDigitID = true;
+
+
+                    foreach (char c in streamRLine)
+                    {
+                        if (char.IsDigit(c) && isDigitID)
+                        {
+                            idBuilder.Append(c);
+                        }
+                        else if (char.IsDigit(c))
+                        {
+                            bitIntegralBuilder.Append(c);
+                        }
+                        else 
+                        { 
+                            isDigitID = false;
+                        }
+                    }
+
+                    int idInt;
+                    UInt16 bitIntegral;
+                    bool parseIdResult = int.TryParse(idBuilder.ToString(), out idInt);
+                    bool parseValueResult = UInt16.TryParse(bitIntegralBuilder.ToString(), out bitIntegral);
+
+
+                    _TUFRegister.Add(new registerDataStruct(idInt, bitIntegral));
+                }
+            }
+        }
+
+        public record struct registerDataStruct(
+            int id,
+            UInt16 bitIntegralValue
+        );
+
+
+    }
+}
