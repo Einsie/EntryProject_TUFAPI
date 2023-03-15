d75778028293b9c642af425e082f621df070d726
Einstainer <32267134+Einstainer@users.noreply.github.com>
2023-03-15 14:57:08 +0200

    Initial comments for TUF and TUFAPIController classes added, as well as in latter documented get requests response types and a check if TUFRegisterList has had elements added to it with new 404 return option

--- EntryProject_TUFAPI/Controllers/TUFAPIController.cs
+++ EntryProject_TUFAPI/Controllers/TUFAPIController.cs
@@ -1,19 +1,58 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿/*
+ * Title: Gambit's TUF-200M ultrasonic energy meter REST API Controller
+ * Description: This API exists to provide users the ability to request data from
+ * Gambit's TUF device to themselves through this API. Data will be converted
+ * into human readable format by the API and sent to user in JSON format.
+ * 
+ * 
+ * Class description: This class receives and controls the handling of request and their return to user
+ * Created: 13.03.2023
+ * 
+ * 
+ * Developer: Albert Kristian Rantala
+ * Last edit: 15.03.2023
+ * Notes: 
+ */
+
+// Using statements necessary for function:
+using Microsoft.AspNetCore.Mvc;
 using EntryProject_TUFAPI.Models;
+
+// Following using statements are in development:
 //using EntryProject_TUFAPI.Models.DTO;
 //using EntryProject_TUFAPI.Data;
 
 namespace EntryProject_TUFAPI.Controllers
 {
-    [Route("api/TUFAPI")]
-    [ApiController]
-    public class TUFAPIController : ControllerBase
+    [Route("api/TUFAPI")]       //Definining the route the API shall use
+    [ApiController]     //Define the type of this class
+    public class TUFAPIController : ControllerBase //this class derives itself from the ControllerBase class
     {
+
+        /* Method: GETTUF
+         * Description: The primary GET request sent to the API that will initiate the necessary tasks of
+         * gaining information from device and return this back to the user with 200 OK response if everything
+         * worked or with 404 not found if TUF no data was found from server
+         * 
+         * HTTPGet: define the type of the method as GET request and provide the possible response types produced
+         * ProducesResponseType: document possible returned response codes
+         * return: this method returns an action result code along with an IEnumerable list consisting of the TUF's Register values
+         * 
+         * Notes:
+         */
         [HttpGet]
-        public IEnumerable<TUF.registerDataStruct> GetTUF()
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public ActionResult<IEnumerable<TUF.registerDataStruct>> GetTUF()
         {
-            TUF newTUF = new TUF();
-            return newTUF.TUFRegisterList;
+            TUF newTUF = new TUF();    // create a new TUF object which will upon creation initiate the process and gain all necessary TUF data
+            if(newTUF.TUFRegisterList.Count == 0)  // check if RegisterList successfully added any elements into its list or if elements inside it remain 0
+            {
+                return NotFound(); // return 404 not found code if no elements exist
+            }
+            return Ok(newTUF.TUFRegisterList); // return ok code along with the nessary list
+
+            // Following is under development:
             //return TUFStore._TUFList;
         }
     }
--- EntryProject_TUFAPI/Models/TUF.cs
+++ EntryProject_TUFAPI/Models/TUF.cs
@@ -1,72 +1,106 @@
-﻿using System.Text;
+﻿/*
+ * Title: Gambit's TUF-200M ultrasonic energy meter REST API Controller
+ * Description: This API exists to provide users the ability to request data from
+ * Gambit's TUF device to themselves through this API. Data will be converted
+ * into human readable format by the API and sent to user in JSON format.
+ * 
+ * 
+ * Class description: This class handles the receival of all necessary data from Gambit's
+ * text file hosted online, parses and compiles this data into date and list of id and value pairs in
+ * local variables. These can then be accessed through accessor properties by other parts of API.
+ * Created: 13.03.2023
+ * 
+ * 
+ * Developer: Albert Kristian Rantala
+ * Last edit: 15.03.2023
+ * notes: 
+ */
+
+// Using statements necessary for function:
+using System.Text;
 using System.Net;
 
 namespace EntryProject_TUFAPI.Models
 {
     public class TUF
     {
-        private List<registerDataStruct> _TUFRegister = new List<registerDataStruct>();
+        // create local class variables
+        private List<registerDataStruct> _TUFRegister;
         private string _date;
 
+        // Class access properties for local variables for defining allowed use for them as only GET
         public string date { get {return _date ;} }
         public List<registerDataStruct> TUFRegisterList { get {return _TUFRegister;} }
 
         public TUF()
         {
+            //initialise local class variables
+            _TUFRegister = new List<registerDataStruct>();
+            _date = "";
+
+            // call GetTUFData method to begin the process of data access and storage into class variables
             GetTUFData();
         }
 
+
+        /* Method: GetTUFData
+         * Description: Internal method which creates an access to the Gambit's online text file containing
+         * TUF data registers, then parses this information into usable values for later conversion
+         * and stores these into appropriate class variables
+         * 
+         * Notes:
+         */
         private void GetTUFData()
         {
-
-            using (WebClient client = new WebClient())
-            using (Stream stream = client.OpenRead("http://tuftuf.gambitlabs.fi/feed.txt"))
-            using (StreamReader streamR = new StreamReader(stream))
+            // using statements which will create the necessary resources for access into and processing Gambit's online TUF Data, existing for duration of method.
+            using (WebClient client = new WebClient()) // creates a webclient required to gain access to online directories over internal ones
+            using (Stream stream = client.OpenRead("http://tuftuf.gambitlabs.fi/feed.txt")) // a path into the Gambit's TUF data online
+            using (StreamReader streamR = new StreamReader(stream)) // streamreader object for processing the information in the given online directory
             {
-                _date = streamR.ReadLine();
+                _date = streamR.ReadLine(); // set _date into TUF data's date, set into first line in Gambit's text file
 
-                while (streamR.Peek() > -1)
+                while (streamR.Peek() > -1) // while loop going through every line in text file in order until end of file
                 {
 
-                    string streamRLine = streamR.ReadLine();
-                    char[] lineResult;
-                    StringBuilder idBuilder = new StringBuilder();
-                    StringBuilder bitIntegralBuilder = new StringBuilder();
-                    bool isDigitID = true;
+                    string streamRLine = streamR.ReadLine(); // go through the next line with Streamreader in text file and store it into temporary string called streamRLine
+                    StringBuilder idBuilder = new StringBuilder(); // a stringbuilder object for separating ID from StreamRLine
+                    StringBuilder bitIntegralBuilder = new StringBuilder(); // a stringbuilder object for sperating the integral value of 16 bits into its own string from StreamRLine
+                    bool isDigitID = true;  // Boolean for confirming if current character in string is still part of ID or not, in text file ID and Value separated by ':' symbol we're looking for
 
-
+                    // foreach loop for going through every individual character stored in streamRLine and determine if the char is a digit or not, if not, set isDigitID to false
                     foreach (char c in streamRLine)
                     {
                         if (char.IsDigit(c) && isDigitID)
                         {
-                            idBuilder.Append(c);
+                            idBuilder.Append(c); // add current character being tested into idBuilder object
                         }
                         else if (char.IsDigit(c))
                         {
-                            bitIntegralBuilder.Append(c);
+                            bitIntegralBuilder.Append(c); // when isDigitID is false, add current character being tested into bitIntegralBuilder Object
                         }
                         else 
                         { 
-                            isDigitID = false;
+                            isDigitID = false; // the ':' symbol separating id and values was found, thus isDigitID is set to false for future foreach loops
                         }
                     }
 
+                    // parse the strings in idBuilder and bitIntegralBuilder and temporarily store them into idInt and bitIntegral respectively
                     int idInt;
                     UInt16 bitIntegral;
                     bool parseIdResult = int.TryParse(idBuilder.ToString(), out idInt);
                     bool parseValueResult = UInt16.TryParse(bitIntegralBuilder.ToString(), out bitIntegral);
 
-
+                    // add a new registerDataStruct into the internal _TUFRegister list with parsed data values
                     _TUFRegister.Add(new registerDataStruct(idInt, bitIntegral));
                 }
             }
         }
 
+
+        // RegisterDataStruct: structure used for storing relevant information into ID and the 16bit IntegralValue pairs
         public record struct registerDataStruct(
             int id,
             UInt16 bitIntegralValue
         );
-
-
     }
 }
